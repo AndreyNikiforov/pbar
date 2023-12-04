d8840f1f19efeafc7793485e8db7131714d6e103
Andrey Nikiforov <andy.nikiforov@gmail.com>
2023-12-03 18:41:17 -0800

    add mp freeze for tqdm

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import click
 import alive_progress
 from rich.progress import track
 from rich.logging import RichHandler
+from multiprocessing import freeze_support
 
 @click.group()
 def commands():
@@ -23,13 +24,36 @@ def commands():
 #         logging.info(f"After Value is {i}")
 
 @commands.command(name="tqdm")
-def tqdm_main():
-    """TQDM as in icloupd"""
+@click.option(
+    "--logging-redirect",
+    help="Use logging redirector or not",
+    default=True
+)
+@click.option(
+    "--freeze-mp",
+    help="Call multiprocessor.freeze_support() or not",
+    default=False
+)
+def tqdm_main(logging_redirect, freeze_mp):
+    """tqdm as in icloupd"""
 
     # works (resizing rearranges already printed log and bar is spilling into the log on resizing)
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
     logger = logging.getLogger("icloudpd")
-    with logging_redirect_tqdm():
+    if (freeze_mp):
+        freeze_support()
+
+    if (logging_redirect):
+        with logging_redirect_tqdm():
+            for i in tqdm.tqdm(range(1,100), 
+                        dynamic_ncols=True,
+                        leave = False,
+                        ascii = True):
+            # for i in range(1,10):
+                logger.debug(f"Before Value is {i}")
+                time.sleep(1)
+                logger.info(f"After Value is {i}")
+    else:
         for i in tqdm.tqdm(range(1,100), 
                     dynamic_ncols=True,
                     leave = False,
@@ -38,6 +62,7 @@ def tqdm_main():
             logger.debug(f"Before Value is {i}")
             time.sleep(1)
             logger.info(f"After Value is {i}")
+
 
 @commands.command(name="alive")
 def alive_main():
@@ -61,7 +86,7 @@ def alive_main():
 @commands.command(name="rich")
 @click.option(
     "--custom-logger",
-    help="Use custom loggging handler or not",
+    help="Use custom logging handler or not",
     default=False
 )
 def rich_main(custom_logger):
